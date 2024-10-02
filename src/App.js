2a0e48c47ceede51ad02cded123b7b5480574bbf
Vladislav Piataikin <vladislav.dev58@gmail.com>
2024-10-02 18:00:18 +0300

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {process.env.TEST}
+        <div>{process.env.TEST}</div>
         Hello
         <p>
           Edit <code>src/App.js</code> and save to reload.
